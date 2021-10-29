4486c973a295c89611b601f437abe30dfd1ae0a0
sebmaz <titan9389@me.com>
2021-10-29 18:41:31 +0400

    changed git actions

--- src/_utils/env.ts
+++ src/_utils/env.ts
@@ -1,5 +1,4 @@
 // TODO : investigate for a workaround (not working https://vitejs.dev/guide/env-and-mode.html#env-variables)
-export const getEnvVar = (key: string) => import.meta.env[key];
+// export const getEnvVar = (key: string) => import.meta.env[key];
 
-export const getApiBaseUrl = (): string =>
-  import.meta.env.VITE_API_BASE_URL as string;
+export const getApiBaseUrl = (): string => import.meta.env.VITE_API_BASE_URL as string;
